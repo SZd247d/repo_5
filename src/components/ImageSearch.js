8ed2d0974abed887150c7fcb9c61acf8824894d2
zakaria sellali <zakisellali464@gmail.com>
2020-11-07 21:57:23 +0100

    Added Some responsiveness

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -9,22 +9,34 @@ const ImageSearch = ({ searchText }) => {
   };
 
   return (
-    <form onSubmit={onSubmit} className='w-full max-w-sm my-10 mx-auto'>
-      <div className='flex items-center border-b border-teal-500 py-2'>
-        <input
-          className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
-          type='text'
-          placeholder='Search Image Term...'
-          aria-label='Full name'
-          onChange={e => setText(e.target.value)}
-        />
-        <button
-          className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
-          type='submit'>
-          Search
-        </button>
+    <div className='md:flex flex-column items-center relative'>
+      <div className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-center text-white py-1 px-2 rounded flex-10 order-2'>
+        <a
+          href='https://github.com/zakisellali/repo_5'
+          target='_blank'
+          rel='noreferrer'>
+          View Code On Github
+        </a>
       </div>
-    </form>
+      <form
+        onSubmit={onSubmit}
+        className=' max-w-sm my-10 mx-auto flex-1 order-1'>
+        <div className='flex items-center border-b border-teal-500 py-2'>
+          <input
+            className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
+            type='text'
+            placeholder='Search Image Term...'
+            aria-label='Full name'
+            onChange={e => setText(e.target.value)}
+          />
+          <button
+            className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
+            type='submit'>
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
